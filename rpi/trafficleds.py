b831aa0558f0b2683304f3573f87881c1577f8a2
Luciano Ramalho <luciano@ramalho.org>
2014-05-11 23:31:57 -0300

    exemplo trafficleds.py

---
+++ rpi/trafficleds.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BOARD)
+
+for pin in [3, 5, 7]:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
+
+for pin in [3, 5, 7]:
+    GPIO.output(pin, 1)
+    sleep(.5)
+    GPIO.output(pin, 0)
+
+GPIO.cleanup()
