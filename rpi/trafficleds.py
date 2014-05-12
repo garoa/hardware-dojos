7c28600f4b41e68146a358051acb8f064b74fe1c
Luciano Ramalho <luciano@ramalho.org>
2014-05-11 23:33:49 -0300

    exemplo trafficleds.py

--- rpi/trafficleds.py
+++ rpi/trafficleds.py
@@ -9,9 +9,10 @@ for pin in [3, 5, 7]:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 0)
 
-for pin in [3, 5, 7]:
-    GPIO.output(pin, 1)
-    sleep(.5)
-    GPIO.output(pin, 0)
+while True:
+    for pin in [3, 5, 7]:
+        GPIO.output(pin, 1)
+        sleep(.5)
+        GPIO.output(pin, 0)
 
 GPIO.cleanup()
